8c5a93bd109c9f863d527b8e53768d4603587f10
berrink <berrin.kizildagli@yh.nackademin.se>
2025-09-07 17:04:49 +0200

    Första 3 punkter är klarad

---
+++ FirstAssigment/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace FirstAssigment
+{
+     class Product
+    {
+      public string Mobile { get;  set; }
+      public int Year { get; set; }
+      public int Price { get; set; }
+
+
+        public Product(string mobile, int year, int price)
+        {
+            Mobile = mobile;
+            Year = year;
+            Price = price;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Telefon: " + Mobile + ", År: " + Year + ", Pris: " + Price + " Kr");
+
+        }
+
+    }
+}
--- FirstAssigment/Program.cs
+++ FirstAssigment/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using FirstAssigment;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Product> products = new List<Product>();
+
+        products.Add(new Product("Samsung Galaxy S20 FE", 2020 , 5000));
+        products.Add(new Product("Iphone 13", 2021, 9000));
+        products.Add(new Product("Xiaomi Mi 11", 2021, 6000));
+
+        foreach(var product in products)
+        {
+            product.PrintInfo();
+        }
+    }
+}
